1b29d190c13714ae42396d269394a12e0cf5448c
Phaneendra Kumar Divi <phaneendra.divi@agilitix.ai>
2018-12-26 15:18:42 +0530

    Fixes.

--- mysqld_exporter/install.sh
+++ mysqld_exporter/install.sh
@@ -7,7 +7,31 @@ OS=$(get_os)
 
 DEFAULT_MYSQL_HOST='localhost'
 DEFAULT_MYSQL_PORT='3306'
-DEFAULT_MYSQL_USER='root'
+DEFAULT_MYSQL_USER='apptuit'
+
+function localize_exporter_config() {
+	echo "The install script will now setup a new user (apptuit) on your MySQL instance for metrics collection"
+	echo -n "MySQL IP [localhost] : "
+	read mysql_host
+	mysql_host=${mysql_host:-${DEFAULT_MYSQL_HOST}}
+	echo -n "MySQL Port [3306] : "
+	read mysql_port
+	mysql_port=${mysql_port:-${DEFAULT_MYSQL_PORT}}
+	echo -n "Password for MySQL 'root' User : "
+	read -s mysql_root_password
+	echo -n "User for metrics collection [apptuit] : "
+	read mysql_user
+	mysql_user=${mysql_user:-${DEFAULT_MYSQL_USER}}
+	echo -n "Setup password for '${mysql_user}' User : "
+	read -s mysql_user_password
+	echo -n "Creating MySQL user '${mysql_user}' and granting needed permissions..."
+	mysql -u root -p${mysql_root_password} mysql -e "CREATE USER '${mysql_user}'@'localhost' IDENTIFIED BY '${mysql_user_password}';"
+	mysql -u root -p${mysql_root_password} mysql -e "GRANT PROCESS ON *.* TO '${mysql_user}'@'localhost';"
+	mysql -u root -p${mysql_root_password} mysql -e "GRANT SELECT ON performance_schema.* TO '${mysql_user}'@'localhost';"
+	mysql -u root -p${mysql_root_password} mysql -e "GRANT REPLICATION CLIENT ON *.* to '${mysql_user}'@'localhost';"
+	echo "Done"
+	sed -e "s/@MYSQL_HOST@/${mysql_host}/" -e "s/@MYSQL_PORT@/${mysql_port}/" -e "s/@MYSQL_USER@/${mysql_user}/" -e "s/@MYSQL_PASSWORD@/${mysql_user_password}/" -i /etc/default/mysqld-exporter
+}
 
 trap 'post_error ${PACKAGE_NAME}' ERR
 check_root
@@ -17,37 +41,14 @@ post_complete $PACKAGE_NAME
 case $OS in
     RedHat)
         install_redhat $PACKAGE_NAME
-        echo -n "MySQL IP [localhost] : "
-        read mysql_host
-        mysql_host=${mysql_host:-${DEFAULT_MYSQL_HOST}}
-        echo -n "MySQL Port [3306] : "
-        read mysql_port
-        mysql_port=${mysql_port:-${DEFAULT_MYSQL_PORT}}
-        echo -n "MySQL User [root] : "
-        read mysql_user
-        mysql_user=${mysql_user:-${DEFAULT_MYSQL_USER}}
-        echo -n "MySQL Password [] : "
-        read -s mysql_password
-        sed -e "s/@MYSQL_HOST@/$mysql_host/" -e "s/@MYSQL_PORT@/${mysql_port}/" -e "s/@MYSQL_USER@/${mysql_user}/" -e "s/@MYSQL_PASSWORD@/${mysql_password}/" -i /etc/default/mysqld-exporter
-
-        service mysqld-exporter start
+        localize_exporter_config
+        start_service $PACKAGE_NAME
         ;;
 
     Debian)
         install_debian $PACKAGE_NAME
-        echo -n "MySQL IP [localhost] : "
-        read mysql_host
-        mysql_host=${mysql_host:-${DEFAULT_MYSQL_HOST}}
-        echo -n "MySQL Port [3306] : "
-        read mysql_port
-        mysql_port=${mysql_port:-${DEFAULT_MYSQL_PORT}}
-        echo -n "MySQL User [root] : "
-        read mysql_user
-        mysql_user=${mysql_user:-${DEFAULT_MYSQL_USER}}
-        echo -n "MySQL Password [] : "
-        read -s mysql_password
-        sed -e "s/@MYSQL_HOST@/$mysql_host/" -e "s/@MYSQL_PORT@/${mysql_port}/" -e "s/@MYSQL_USER@/${mysql_user}/" -e "s/@MYSQL_PASSWORD@/${mysql_password}/" -i /etc/default/mysqld-exporter
-        service mysqld-exporter start
+        localize_exporter_config
+        start_service $PACKAGE_NAME
         ;;
 
     *)
