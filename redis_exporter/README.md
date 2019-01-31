c099c29db7a51690220ee907460fefccc048dd56
Phaneendra Kumar Divi <phaneendra.divi@agilitix.ai>
2019-01-31 08:56:57 +0530

    First cut redis exporter.

---
+++ redis_exporter/README.md
@@ -0,0 +1,20 @@
+# Redis Metrics Exporter [![Build Status](https://travis-ci.com/phakudi/prometheus-exporters.svg?branch=master)](https://travis-ci.com/phakudi/prometheus-exporters)
+
+Prometheus Redis metrics exporter for reporting metrics to Apptuit.ai
+
+### Installation
+
+Redis Exporter is available via debian & yum repositories. The installer script 
+can be invoked as follows...
+
+For installation using curl, please invoke...
+ 
+```
+bash -c "$(curl -Ls https://raw.githubusercontent.com/phakudi/prometheus-exporters/master/redis_exporter/install.sh)"
+``` 
+
+For installation using wget, please invoke...
+
+```
+bash -c "$(wget -qO- https://raw.githubusercontent.com/phakudi/prometheus-exporters/master/redis_exporter/install.sh)"
+```
---
+++ redis_exporter/install.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+if [ -r ../common.sh ]; then
+	. ../common/common.sh
+else
+	curl=$(which curl)
+	r=$?
+	if [ $r == 0 ]; then
+		$curl -o /tmp/common.sh https://raw.githubusercontent.com/phakudi/prometheus-exporters/master/common/common.sh
+	else
+		wget=$(which wget)
+		r=$?
+		if [ $r == 0 ]; then
+			$wget -O /tmp/common.sh https://raw.githubusercontent.com/phakudi/prometheus-exporters/master/common/common.sh
+		else
+			echo "Neither 'curl' nor 'wget' found. Please install at least one of these packages."
+			exit 1
+		fi
+	fi
+	. /tmp/common.sh
+fi
+
+PACKAGE_NAME='redis-exporter'
+OS=$(get_os)
+
+function configure_exporter() {
+	print_message "info" "No localization needed for redis-exporter"
+	return
+}
+
+trap 'post_error ${PACKAGE_NAME}' ERR
+check_root
+setup_log $PACKAGE_NAME
+post_complete $PACKAGE_NAME
+
+case $OS in
+    RedHat)
+        install_redhat $PACKAGE_NAME
+        configure_exporter $0
+        start_service $PACKAGE_NAME
+        ;;
+
+    Debian)
+        install_debian $PACKAGE_NAME
+        configure_exporter $0
+        start_service $PACKAGE_NAME
+        ;;
+
+    *)
+        print_message "error" "Your OS/distribution is not supported by this install script.\n"
+        exit 1;
+        ;;
+esac
